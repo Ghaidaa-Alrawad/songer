968760f3721d91c9820b2125d56a650373a10dea
Ghaidaa-Alrawad <ghaidaarowad@gmail.com>
2023-09-04 15:37:42 +0000

    initial commit from Spring init

---
+++ src/main/java/com/songer/songerWebApp/SongerWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.songer.songerWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongerWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongerWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/songer/songerWebApp/SongerWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songer.songerWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongerWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
