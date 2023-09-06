3562e8cfc32aab45fb05c973592f4f756745ef74
Ghaidaa-Alrawad <ghaidaarowad@gmail.com>
2023-09-06 19:07:31 +0000

    lab12

--- src/main/java/com/songer/songerWebApp/controller/AlbumController.java
+++ src/main/java/com/songer/songerWebApp/controller/AlbumController.java
@@ -1,19 +1,38 @@
 package com.songer.songerWebApp.controller;
 
 import com.songer.songerWebApp.model.Album;
+import com.songer.songerWebApp.repositores.AlbumRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
 
 @Controller
 public class AlbumController {
+    @Autowired
+    AlbumRepo albumRepo;
+    // ("Midnight", "Band: Set It Off", 15, 43, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS31QEAIPhEdJq4jVskU5hDnqWYc6rhU_S9IQ&usqp=CAU");
+    // ("HELP EVER HURT NEVER", "Artist: Fujii Kaze", 11, 46, "https://i.scdn.co/image/ab67616d0000b27322805a1b17e41ae357bd98bc");
+    // ("Death of a Bachelor", "Band: Panic! At The Disco", 11, 35, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT68KmyZtBlXUtEONPJBpqUNzOrmddKKJsgKg&usqp=CAU");
+    // ("Wish You Were Here", "Pink Floyd", 5, 44, "https://faroutmagazine.co.uk/static/uploads/1/2019/05/%E2%80%98Wish-You-Were-Here%E2%80%99-%E2%80%93-Pink-Floyd-cover-meaning.jpg");
+
 
     @GetMapping("/albums")
-    public String albums(Model albumM){
-        Album album1 = new Album("Midnight", "Band: Set It Off", 15, 43, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS31QEAIPhEdJq4jVskU5hDnqWYc6rhU_S9IQ&usqp=CAU");
-        Album album2 = new Album("HELP EVER HURT NEVER", "Artist: Fujii Kaze", 11, 46, "https://i.scdn.co/image/ab67616d0000b27322805a1b17e41ae357bd98bc");
-        Album album3 = new Album("Death of a Bachelor", "Band: Panic! At The Disco", 11, 35, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT68KmyZtBlXUtEONPJBpqUNzOrmddKKJsgKg&usqp=CAU");
-        albumM.addAttribute("albums",new Album[]{album1,album2,album3});
+    public String albums(Model albumM) {
+        List<Album> albums = albumRepo.findAll();
+        System.out.println(albums.toString());
+        albumM.addAttribute("albums",albums);
         return "album.html";
+    }
+
+    @PostMapping("/albums/creat-album")
+    public RedirectView creatingAlbums(String title, String artist, int songCount, int length, String imgUrl){
+        Album newAlbum = new Album(title, artist, songCount, length, imgUrl);
+        albumRepo.save(newAlbum);
+        return new RedirectView("/albums");
     }
 }
--- src/main/java/com/songer/songerWebApp/model/Album.java
+++ src/main/java/com/songer/songerWebApp/model/Album.java
@@ -1,6 +1,17 @@
 package com.songer.songerWebApp.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
     private String title;
     private String artist;
@@ -16,23 +27,53 @@ public class Album {
         this.imgUrl = imgUrl;
     }
 
+    public Album() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getArtist() {
         return artist;
     }
 
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
     public int getSongCount() {
         return songCount;
+    }
+
+    public void setSongCount(int songCount) {
+        this.songCount = songCount;
     }
 
     public int getLength() {
         return length;
     }
 
+    public void setLength(int length) {
+        this.length = length;
+    }
+
     public String getImgUrl() {
         return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
     }
 }
